4caebc82ae24b6de939735a57f234c9319825fd3
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2018-05-21 17:38:05 +0200

    Add carousel made with JS - mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+'use strict';
+
+var templateSlides = document.getElementById('template-slides').innerHTML;
+Mustache.parse(templateSlides);
+
+var listSlides = '';
+
+for (var i = 0; i < SlideList.length; i++) {
+	console.log(SlideList);
+	listSlides += Mustache.render(templateSlides, SlideList[i]);
+}
+
+var slidesContainer = document.getElementById('slides-container')
+
+slidesContainer.insertAdjacentHTML('beforeend', listSlides)
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
