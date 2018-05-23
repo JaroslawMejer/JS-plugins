8fe298ac405b98205bc555272acb1dd1157312d2
Jarosław Mejer <jaroslaw.mejer@gmail.com>
2018-05-23 13:48:03 +0200

    Finish gmap with markers

--- js/script.js
+++ js/script.js
@@ -33,33 +33,17 @@ flkty.on( 'scroll', function( progress ) {
 
 window.initMap = function() {
         var greenLand = {lat: 71.706936, lng: -42.604303000000016};
-        var puszczaPoland = {lat: 52.7228852, lng: 23.655567399999995};
-        var miamiBeach = {lat: 25.790654, lng: -80.1300455};
-        var wroclawCity = {lat: 51.1078852, lng: 17.03853760000004};
-        var iceland = {lat: 64.963051, lng: -19.020835000000034};
         
         var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 4,
           center: greenLand
         });
 
-        var contentString = '<div id="content">'+
-            '<h1 id="firstHeading" class="firstHeading">Greenland</h1>'+
-            '<div id="bodyContent">'+
-            '<p><b>Greenland</b>, amazing sky <b>full of colors</b>.'+
-            '</div>'+
-            '</div>';
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
-
-        var marker = new google.maps.Marker({
-          position: greenLand,
-          map: map,
-          title: 'Greenland'
-        });
-
-        marker.addListener('click', function() {
-          infowindow.open(map, marker);
-        });
+        for (var i = 0; i < SlideList.length; i++) {
+          var marker = new google.maps.Marker({
+            position: SlideList[i].coords,
+            map: map,
+            title: SlideList[i].title
+          });
+        }
       }
